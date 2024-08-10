5f0df5239e5aaf24517bdea70aa0f195006ea006
nabilChemkhi <nabil.chemkhi@esprit.tn>
2024-08-10 23:38:14 +0100

    boxes

--- src/app/components/box/box.component.ts
+++ src/app/components/box/box.component.ts
@@ -1,20 +1,20 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { Box } from '../../models/model';
+// import { Component, EventEmitter, Input, Output } from '@angular/core';
+// import { Box } from '../../models/model';
 
-@Component({
-  selector: 'app-box',
-  standalone: true,
-  imports: [],
-  templateUrl: './box.component.html',
-  styleUrl: './box.component.css'
-})
-export class BoxComponent {
-  @Input() box!: Box;
-  @Input() isSelected!: boolean;
-  @Output() selectBox = new EventEmitter<void>();
+// @Component({
+//   selector: 'app-box',
+//   standalone: true,
+//   imports: [],
+//   templateUrl: './box.component.html',
+//   styleUrl: './box.component.css'
+// })
+// export class BoxComponent {
+//   @Input() box!: Box;
+//   @Input() isSelected!: boolean;
+//   @Output() selectBox = new EventEmitter<void>();
 
-  onSelectBox(): void {
-    this.selectBox.emit();
-  }
+//   onSelectBox(): void {
+//     this.selectBox.emit();
+//   }
 
-}
+// }
